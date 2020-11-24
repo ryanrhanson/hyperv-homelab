9f7b091c839653c0fd9a79b146b02f4fb94a00ba
Ryan Hanson <ryan@megacosm.net>
2020-11-24 00:10:17 -0600

    Added help, adjusted prep function slightly.

--- homelab.sh
+++ homelab.sh
@@ -29,6 +29,20 @@ function list_vms () {
 	. homelab_scripts/list_vms.sh
 }
 
+function prep_vm () {
+        . homelab_scripts/get_vm_state.sh
+        if [[ ${VM_STATE} != 'Running' ]]; then
+         echo "${VM_NAME} is not currently running. Please start the vm and wait at least ${SLEEP_TIME} seconds."
+         exit 1
+        fi
+        . homelab_scripts/get_vm_ip.sh
+        if [[ -z ${VM_IP} ]]; then
+          echo "Could not get an IP for ${VM_NAME} - Please try again later or log in through the Hyper-V Console to troubleshoot manually."
+          exit 1
+        fi
+        . homelab_scripts/prep_vm.sh
+}
+
 function start_vm () {
         . homelab_scripts/start_vm.sh
         echo "Waiting ${SLEEP_TIME} seconds for vm to boot."
@@ -38,9 +52,11 @@ function start_vm () {
           prep_vm
         fi
 }
+
 function stop_vm () {
         . homelab_scripts/stop_vm.sh
 }
+
 function console_vm () {
         vmconnect.exe localhost "${VM_NAME}"
 }
@@ -51,20 +67,6 @@ function provision_vm () {
 	if [[ ${START_VM} -eq 1 ]]; then
 	  start_vm
 	fi
-}
-
-function prep_vm () {
-	. homelab_scripts/get_vm_state.sh
-	if [[ ${VM_STATE} != 'Running' ]]; then
-	 echo "${VM_NAME} is not currently running. Please start the vm and wait at least 30 seconds."
-	 exit 1
-	fi
-	. homelab_scripts/get_vm_ip.sh
-	if [[ -z ${VM_IP} ]]; then 
-	  echo "Could not get an IP for ${VM_NAME} - Please try again later or log in through the Hyper-V Console to troubleshoot manually."
-	  exit 1
-	fi
-	. homelab_scripts/prep_vm.sh
 }
 
 function modify_vm () {
@@ -99,6 +101,47 @@ function forward_wsl() {
 	echo "$(cat homelab_scripts/talk_wsl.ps1)"
 	echo "=========="
 }
+
+function show_help() {
+
+cat << EOF
+Usage: homelab.sh command [options]
+Commands: provision, start, stop, prep, destroy, list, templates, mod_template, login, console, wsl_forward
+
+    provision            Provision a VM, optionally starting and prepping it
+    start                Start a VM
+    stop                 Stop a VM
+    prep                 Prep VM for login (copy key, set hostname) - Must be running
+    destroy              Delete a VM
+    login                Display login information
+    console              Run vmconnect to open Hyper-V console for given VM
+    list                 List all current Hyper-V VMs
+    templates            List available template files
+    mod_template         Modify attributes for the given template
+    wsl_forward          Display powershell command to enable forwarding between WSL and Hyper-V guests
+
+    General Arguments:
+    -h|--help            Display this helpfile
+    -n|--name            VM Name - Usable with provision, start, stop, prep, destroy, login, console
+    -t|--template        Template File - Usable with provision, mod_template
+    -c|--cores           Number of virtual cpu cores - Usable with provision
+    -m|--memory          VM Ram allocation in MB (ie 3072 for 3GB) - Usable with provision
+    -s|--start           Run VM start after provision - Usable with provision
+    -p|--prep            Run VM prep after start - Usable with provision, start
+    --distrover          Set the 'distrover' attribute on the given template - Usable with mod_template
+    --description        Set the 'description' attribute on the given template - Usable with mod_template
+
+Example: ./homelab.sh provision -n "Cent8 Test" -p -s -m 2048 -c 2 -t "Cent8-Base.vhdx"
+
+The above will provision a VM named "Cent8 Test" with 2 vCPUs and 2GB RAM from the "Cent8-Base.vhdx" template.
+It will also start the VM after provisioning, and run the prep function (copy key in and set hostname).
+
+Example: ./homelab.sh mod_template --distrover "CentOS 7" --description "Base Image" -t "Cent7-Base.vhdx"
+
+The above will set the distrover to "CentOS 7" and description to "Base Image" on the "Cent7-Base.vhdx" template file.
+EOF
+#
+} 
 
 while [[ $# -gt 0 ]]
   do
